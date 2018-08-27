601efa527bf2f15661dce7ae3033e4ac693e0b9f
Josce Jaimes <nanayhachi94@gmail>
2018-08-27 12:45:06 -0500

    Subiendo login y estilos

---
+++ src/js/credentials.js
@@ -0,0 +1,11 @@
+  //Credenciales
+  // Initialize Firebase 
+  var config = {
+    apiKey: "AIzaSyDO5Wew-j_St0nV2SKI_2olEHHgR56gaUs",
+    authDomain: "movies-blog.firebaseapp.com",
+    databaseURL: "https://movies-blog.firebaseio.com",
+    projectId: "movies-blog",
+    storageBucket: "movies-blog.appspot.com",
+    messagingSenderId: "447853010961"
+  };
+  firebase.initializeApp(config);
