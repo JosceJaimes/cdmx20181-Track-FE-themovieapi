d6737b9cc6260b0595f8e5d099de17cb132f3b98
joscejaimes <nanayhachi94@gmail.com>
2019-07-08 13:36:29 -0500

    modificando muestra de informacion de peliculas

--- src/js/data.js
+++ src/js/data.js
@@ -76,6 +76,36 @@ window.painter = (data)=>{
         <div class="card">
         <div class = "estudiante">
         <p><img src="${movie.Poster}"></p>
+        <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#${movie.imdbID}">
+        <button>
+        ver datos
+        </button>
+      </a>
+
+        <div class = "card">
+      
+      <div class="modal fade" id="${movie.imdbID}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel"></h5>
+              <button type="button" claxss="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+                </div>
+               <div class="modal-body">
+               <p>Nombre: ${movie.Title}</p>
+               <p>Año: ${movie.Year}</p>
+               <p>Tipo: ${movie.Type}</p>
+               <p><img src="${movie.Poster}"></p>
+               </div>
+             <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>            
+             </div>
+          </div> 
+        </div>
+      </div>
+        </button>
         </div>
         </div>
         </div> `;
